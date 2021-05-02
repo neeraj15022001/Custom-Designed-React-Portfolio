7eb38b3fd043b95ae83a723dc998da938176a6f5
neeraj15022001 <gneeraj32595@gmail.com>
2021-05-02 17:10:10 +0530

    changed tech in home

--- src/components/Pages/Main/Main.jsx
+++ src/components/Pages/Main/Main.jsx
@@ -14,7 +14,7 @@ function Main() {
                 <Row>
                     <Col xs={12} lg={6} className="d-flex align-items-start flex-column">
                         <div className="d-flex align-items-center justify-content-center flex-fill">
-                        <p className="display-4 fw-bolder mb-5 text-tertiary">Hello my<br/> name is <span className="text-theme">Avishka</span>.<br/> I am a Full Stack<br/> Developer</p>
+                        <p className="display-4 fw-bolder mb-5 text-tertiary">Hello my<br/> name is <span className="text-theme">Avishka</span>.<br/> I am a FrontEnd<br/> Developer</p>
                         </div>
                         <div className="py-3 text-tertiary">
                             <EmailIcon className="fs-1" />
