b5a4f853b50b5f6ed3bd2aebdaf777952c8f9774
precious-adeyinka <pflash90@gmail.com>
2021-10-05 18:18:38 +0100

    refactor: Optimize responsiveness across all viewports and screen sizes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Portfolio Template
 # Designed by Neeraj Gupta
+
+### Setup from source code
+Follow the steps below
+
+- Fork the repo on github (https://github.com/neeraj15022001/Custom-Designed-React-Portfolio.git)
+- Clone your fork to your local system 
+- cd Custom-Designed-React-Portfolio
+- npm install
+- npm start
--- src/components/Pages/Skills/Skills.jsx
+++ src/components/Pages/Skills/Skills.jsx
@@ -3,23 +3,26 @@ import CustomNavbar from "../../common/CustomNavbar/CustomNavbar";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import skills from "../../../resources/skills.png";
 import Donut from "./Donut Chart/Donut";
+import "./Skills.css";
 
 function Skills() {
   return (
     <div>
       <CustomNavbar colorClass="text-theme" activePage="/skills" />
       <Container>
-        <Row>
+        <Row style={{ height: "auto" }}>
           <Col
+            xs={12}
             lg={7}
-            className="d-flex align-items-center justify-content-center flex-column text-center"
+            className="d-flex align-items-center justify-content-center flex-column text-center illustrationOne"
           >
             {/* <p className="display-1 fw-bold text-center m-0 text-tertiary">Skills</p> */}
             <Donut />
           </Col>
           <Col
+            xs={12}
             lg={5}
-            className="d-flex align-items-center justify-content-center"
+            className="d-flex align-items-center justify-content-center illustrationTwo"
             style={{ height: "90vh" }}
           >
             <Image
